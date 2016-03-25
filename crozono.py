02004fb6324f615f1909bfc3b2560682ccf3cc6f
nvillanueva <villanueva.arg@gmail.com>
2016-03-25 17:15:25 -0300

    added emphasis

--- crozono.py
+++ crozono.py
@@ -6,8 +6,7 @@
 	CROZONO - 22.02.16.20.00.00 - www.crozono.com - info@crozono.com
 ----------------------------------------------------------------------------
 
-!!! The authors are not responsible for any misuse of the application.
-asdadasdasdad
+!!! The authors are not responsible for any misuse of the application !!!
 
 """
 
